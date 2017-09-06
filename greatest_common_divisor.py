f7ef0fd7c785660eed41098e3be4e6af322d469c
wilkenshuang <wilkenshuang@gmail.com>
2017-09-06 23:45:06 +0800

    Update greatest_common_divisor.py

--- greatest_common_divisor.py
+++ greatest_common_divisor.py
@@ -5,6 +5,11 @@
 # @File    : test2.py
 # @Software: PyCharm Community Edition
 
+'''
+寻找两个数的最大公因子，解法用到了欧几里得算法，即令r0= a，r1 = b，然后计算相继的商和余数，
+直到rn+1为0，最后的非零余数rn就是a与b的最大公因子。
+'''
+
 def biggestFactor(a,b):
     r0=max(a,b)
     r1=min(a,b)
