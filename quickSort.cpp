aaa42c1d856b961e7b55a920d3178fc057fc67e7
wilkenshuang <wilkenshuang@gmail.com>
2018-06-26 11:27:52 +0800

    Update quickSort.cpp

--- quickSort.cpp
+++ quickSort.cpp
@@ -8,6 +8,7 @@
 
 using namespace std;
 void quickSort(int a[], int, int);
+void insertSort(int arr[],int n);
 
 int main() {
 	int arr[] = { 34,65,12,43,67,5,78,10,3,70 };
@@ -34,5 +35,16 @@ void quickSort(int a[],int l, int r){
 		a[i] = v;
 		quickSort(a, 0, i - 1);
 		quickSort(a, i + 1, r);
+	}
+}
+void insertSort(int arr[],int n){
+	for (int i=1;i<n;i++){
+		int j=i;
+		while (j>=1&&arr[j-1] > arr[j]){
+			int tmp = arr[j-1];
+			arr[j-1] = arr[j];
+			arr[j] = tmp;
+			j--;
+		}
 	}
 }
